3aa35cadbfde7e526b494fbf121766f4be13039f
AlexanderMiller347 <am5389@drexel.edu>
2025-07-30 09:55:56 -0400

    Updated Tic-tac-toe

--- Tic Tac Toe/tic-tac-toe.cpp
+++ Tic Tac Toe/tic-tac-toe.cpp
@@ -6,6 +6,8 @@
 using namespace std;
 
 void printBoard(char array[][3], int height, int width);
+string checkWin(char array[][3], int height, int width);
+
 void CPUMove();
 int main() {
     
@@ -15,36 +17,62 @@ int main() {
     };
     int height = sizeof(board)/sizeof(board[0]);
     int width = sizeof(board[0])/sizeof(board[0][0]);
+    
     int playerMove;
     int computerMoves[] = {11,12,13,21,22,23,31,32,33};
+    int moveCount = 0;
     bool cpuValid = false;
-    string winner = "None";
+    bool playerValid = false;
+    
+    string winner = "Tie";
 
     do{
-        cout << "Here is the board:\n\n"; 
-        printBoard(board,height,width);
-        cout << "\nMake your move (11-33): ";
-        cin >> playerMove;
-        
-        if(board[(playerMove/10) - 1][(playerMove%10) - 1] == ' '){
-            board[(playerMove/10) - 1][(playerMove%10) - 1] = 'X';
-        }
-        else{
-            cout << "Invalid Move\n";
-        }
-        
+
         do{
+            cout << "Here is the board:\n\n"; 
+            printBoard(board,height,width);
+            cout << "\nMake your move (11-33): ";
+            cin >> playerMove;
+            
+            if(board[(playerMove/10) - 1][(playerMove%10) - 1] == ' '){
+                board[(playerMove/10) - 1][(playerMove%10) - 1] = 'X';
+                playerValid = true;
+                moveCount++;
+            }
+            else{
+                cout << "Invalid Move\n\n";
+            }
+        }while(playerValid == false);
+        playerValid = false;
+        winner = checkWin(board,height,width);
+
+        if(moveCount < 9 && winner == "Tie"){
+            do{
             srand(time(0));
             int move = computerMoves[rand() % 9];
             if(board[(move/10) - 1][(move%10) - 1] == ' '){
                     board[(move/10) - 1][(move%10) - 1] = 'O';
                     cpuValid = true;
+                    moveCount++;
             }
-        }while(cpuValid == false);
-        cpuValid = false;
-    }while(winner == "None");
+            }while(cpuValid == false);
+            cpuValid = false;
+            winner = checkWin(board,height,width);
+        }
+        
+    }while(winner == "Tie" && moveCount != 9);
 
-
+    cout << '\n';
+    printBoard(board,width,height);
+    if(winner == "Player"){
+        cout << "\nCongratulations! You Win!";
+    }
+    else if(winner == "CPU"){
+        cout << "\nComputer Wins.";
+    }
+    else{
+        cout << "\nIt's a tie.";
+    }
     return 0;
 }
 
@@ -56,4 +84,39 @@ void printBoard(char array[][3], int height, int width){
         }
         cout << '\n';
     }
+}
+string checkWin(char array[][3], int height, int width){
+
+    for(int i = 0; i < height; i++){
+        if(array[i][0] == array[i][1] && array[i][1] == array[i][2]){
+            if(array[i][0] == 'X'){
+                return "Player";
+            }
+            else if(array[i][0] == 'O'){
+                return "CPU";
+            }
+        }
+
+    }
+
+    for(int j = 0; j < width; j++){
+        if(array[0][j] == array[1][j] && array[1][j] == array[2][j]){
+            if(array[0][j] == 'X'){
+                return "Player";
+            }
+            else if(array[0][j] == 'O'){
+                return "CPU";
+            }
+        }
+    }
+
+    if((array[0][0] == array[1][1] && array[1][1] == array[2][2])||(array[0][2] == array[1][1] && array[1][1] == array[2][0])){
+        if(array[1][1] == 'X'){
+            return "Player";
+        }
+        else if(array[1][1] == 'O'){
+            return "CPU";
+        }
+    }
+    return "Tie";
 }
